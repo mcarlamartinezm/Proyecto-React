914713387325b7d4199db4723386c18e0a6cc1e4
mcarlamartinezm <mcarla@gmail.com>
2025-09-23 00:18:52 -0300

    ProyectoFinalMartinez

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -3,7 +3,7 @@ function Inicio({ addToCart }) {
   return (
     <>
       <h1 className="titulo">Poketienda</h1> 
-      <p id="bienve-text">Encuentra los mejores Pokemones de compañía.<br /> Puedes ver en la pokedex las caracteristicas de cada uno y agregar al carrito de compras a tu nuevo compañero.
+      <p className="bienve-text">Encuentra los mejores Pokemones de compañía.<br /> Puedes ver en la pokedex las caracteristicas de cada uno y agregar al carrito de compras a tu nuevo compañero.
      <br /> ¡Para que te acompañen en la aventura!</p>
       <ItemListDestacados addToCart={addToCart} />
     </>
--- src/pages/Pokemones.jsx
+++ src/pages/Pokemones.jsx
@@ -4,6 +4,7 @@ function Pokemones({ addToCart }) {
   return (
     <>
       <h1 className="titulo">Elije tus Pokemones</h1>
+      <p className='bienve-text'>Son solo 150 o más que ver, escoge a tu amigo de aventura.</p>
       <ItemListContainer addToCart={addToCart} />
     </>
   );
